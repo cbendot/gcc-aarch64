2390a8fec3758fc6914cfc5426e605d52a14b527
ben863 <r.budhiono@gmail.com>
2022-06-20 16:12:41 +0700

    Import aarch64--glibc--bleding-edge-2021.11-1 from https://toolchains-bootlin.com/ GCC Version      : 11.2.0 GDB Version      : 11.1 Binutils Version : 2.37 Linux Headers    : 5.4.163 glibc            : 2.34-9-g9acab0bba6a5a57323b1f94bf95b21618a9e5aa4
    Change-Id: Idfbdb6cccc5fdaefe438cbccad80e92f33adc605
    Co-authored-by: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: ben863 <r.budhiono@gmail.com>

---
+++ lib/gcc/aarch64-buildroot-linux-gnu/11.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/builds/bootlin/toolchains-builder/build/aarch64--glibc--bleeding-edge-2021.11-1 --sysconfdir=/builds/bootlin/toolchains-builder/build/aarch64--glibc--bleeding-edge-2021.11-1/etc --enable-static --target=aarch64-buildroot-linux-gnu --with-sysroot=/builds/bootlin/toolchains-builder/build/aarch64--glibc--bleeding-edge-2021.11-1/aarch64-buildroot-linux-gnu/sysroot --enable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --disable-decimal-float --with-gmp=/builds/bootlin/toolchains-builder/build/aarch64--glibc--bleeding-edge-2021.11-1 --with-mpc=/builds/bootlin/toolchains-builder/build/aarch64--glibc--bleeding-edge-2021.11-1 --with-mpfr=/builds/bootlin/toolchains-builder/build/aarch64--glibc--bleeding-edge-2021.11-1 --with-pkgversion='Buildroot toolchains.bootlin.com-2021.11-1' --with-bugurl=http://bugs.buildroot.net/ --without-zstd --disable-libquadmath --disable-libquadmath-support --enable-tls --enable-threads --without-isl --without-cloog --with-abi=lp64 --with-cpu=cortex-a53 --enable-languages=c,c++,fortran --with-build-time-tools=/builds/bootlin/toolchains-builder/build/aarch64--glibc--bleeding-edge-2021.11-1/aarch64-buildroot-linux-gnu/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "lp64" }, { "cpu", "cortex-a53" } };
